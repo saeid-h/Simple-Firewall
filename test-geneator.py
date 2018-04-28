2d46d0b74d51414ebdcaf2bf0724cc3c303e6b5c
Saied <shossei1@stevens.edu>
2018-04-28 12:51:39 -0400

    Update test-geneator.py
    The procedure is updated such that generates single IPs for source and destinations. A random variable decides if repeat the latest traffic, pick one of the previous traffics, or generated a new traffic. The probability for picking from history is 30%, for repeating the latest one is 50%, and for new traffic is 20%.

--- test-geneator.py
+++ test-geneator.py
@@ -7,65 +7,39 @@ import random
 
 random.seed(1000)
 
-def generateIP ():
-	p = random.randint(0,50)
-	if not p:
-		a = str(random.randint(1,255))
-		b = str(random.randint(0,255))
-		c = str(random.randint(0,255))
-		d = str(random.randint(0,255))
-		e = str(4*random.randint(1,6))
-	else:
-		e = 0
-		p = random.randint(0,50)
-		if not p:
-			a = '*'
-			b = '*'
-			c = '*'
-			d = '*'
-		else:
-			a = str(random.randint(1,255))
-			p = random.randint(0,30)
-			if not p:
-				b = '*'
-				c = '*'
-				d = '*'
-			else:
-				b = str(random.randint(0,255))
-				p = random.randint(0,20)
-				if not p:
-					c = '*'
-					d = '*'
-				else:
-					c = str(random.randint(0,255))
-					p = random.randint(0,10)
-					if not p:
-						d = '*'
-					else:
-						d = str(random.randint(0,255))
-
-	if not e:
-		ip = str(a) + '.' + str(b) + '.' + str(c) + '.' + str(d)
-	else:
-		ip = str(a) + '.' + str(b) + '.' + str(c) + '.' + str(d) + '/' + str(e)
+def generateTrafficIP ():
+	a = str(random.randint(1,255))
+	b = str(random.randint(0,255))
+	c = str(random.randint(0,255))
+	d = str(random.randint(0,255))
+	ip = str(a) + '.' + str(b) + '.' + str(c) + '.' + str(d)
+		
 	return ip
 
 
 
-ruleFile = open('TestIP.csv', 'w')
+trafficFile = open('TestIP.csv', 'w')
 
-Rules = []
-for i in range(100):
-	p = random.randint(0,4)
-#	Action = 'Block' if p else 'Allow'
-	Rules.append([generateIP(), generateIP()])
+Traffics = []
+t = [generateIP(), generateIP()]
+Traffics.append(t)
+for i in range(1000):
+	p = random.randint(1,10)
+	if p < 3:
+		i = random.randint(0,len(Traffic)-1)
+		t = Traffic[i]
+	elif p < 8:
+		t = Traffic[-1]
+	else:
+		t = [generateIP(), generateIP()]
+	Traffics.append(t)
 
-print (Rules)
+print (Traffics)
 
 
-with ruleFile:
-    writer = csv.writer(ruleFile)
-    writer.writerows(Rules)
+with trafficFile:
+    writer = csv.writer(trafficFile)
+    writer.writerows(Traffics)
 
 print("Writing complete")
 
